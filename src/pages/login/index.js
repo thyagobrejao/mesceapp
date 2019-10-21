c5a026d8db3805e206a6c3d7eb4ee5fedc94eb29
Thyago <thyago.brejao@gmail.com>
2019-10-21 16:42:03 -0300

    Tela de Login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,34 +1,107 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
-import { Container, Header, Content, Form, Item, Input, Label, Left, Title, Subtitle, Body, Right } from 'native-base';
+import { Image, StyleSheet } from 'react-native';
+import { Container, Header, Content, Form, Item, Input, Label, Left, Title, Body, Right, Button, Text } from 'native-base';
+import { StackActions, NavigationActions } from 'react-navigation';
+
 export default class Login extends Component {
+  static navigationOptions = {
+    header: null,
+  };
+  state = {
+    email: '',
+    password: '',
+    error: '',
+  };
+  handleEmailChange = (email) => {
+    this.setState({ email });
+  };
+  handlePasswordChange = (password) => {
+    this.setState({ password });
+  };
+  handleSignInPress = async () => {
+    if (this.state.email.length === 0 || this.state.password.length === 0) {
+      this.setState({ error: 'Preencha usuário e senha para continuar!' }, () => false);
+    } else {
+      try {
+        const response = await api.post('/sessions', {
+          email: this.state.email,
+          password: this.state.password,
+        });
+
+        const resetAction = StackActions.reset({
+          index: 0,
+          actions: [
+            NavigationActions.navigate({ routeName: 'Main', params: { token: response.data.token } }),
+          ],
+        });
+        this.props.navigation.dispatch(resetAction);
+      } catch (_err) {
+        this.setState({ error: 'Houve um problema com o login, verifique suas credenciais!' });
+      }
+    }
+  };
   render() {
+
     return (
-      <Container>
+      <Container style={{
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "#ecf0f1",
+      }}>
         <Header>
           <Left />
           <Body>
-            <Title>Title</Title>
+            <Title>MESCE.com.br</Title>
           </Body>
           <Right />
         </Header>
         <Content>
-          {/* <Image
-            style={{ width: '50%', height: '50%' }}
-            source={require('../../images/distribuindo-comunhao-transparente2.png')} 
-          /> */}
+          <Image
+            style={{ marginTop: 30, marginBottom: 20, height: 300, width: null, flex: 1 }}
+            source={require('../../images/distribuindo-comunhao-transparente2.png')}
+          />
           <Form>
             <Item floatingLabel>
-              <Label>Username</Label>
-              <Input />
+              <Label>Endereço de e-mail</Label>
+              <Input
+                style={styles.loginInput}
+                value={this.state.email}
+                onChangeText={this.handleEmailChange}
+                autoCapitalize="none"
+                autoCorrect={false}
+              />
             </Item>
             <Item floatingLabel last>
-              <Label>Password</Label>
-              <Input />
+              <Label>Senha</Label>
+              <Input
+                style={styles.loginInput}
+                value={this.state.password}
+                onChangeText={this.handlePasswordChange}
+                autoCapitalize="none"
+                autoCorrect={false}
+                secureTextEntry
+              />
             </Item>
+            <Button block style={styles.loginButton}>
+              <Text>Entrar</Text>
+            </Button>
           </Form>
         </Content>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  loginInput: {
+    borderRadius: 5,
+    width: "90%",
+    marginRight: 20,
+  },
+
+  loginButton: {
+    marginTop: 30,
+    width: "90%",
+    marginLeft: 20,
+  },
+});
--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -1,34 +1,13 @@
-import styled from 'styled-components';
+'use strict';
+import { React, StyleSheet } from 'react-native';
 
-const Container = styled.View`
-  flex: 1;
-  alignItems: center;
-  justifyContent: center;
-  backgroundColor: #f5f5f5;
-`;
-
-const Image = styled.Image`
-  height: 30%;
-  marginBottom: 40px;
-`;
-
-const Input = styled.TextInput`
-  paddingHorizontal: 20px;
-  paddingVertical: 15px;
-  borderRadius: 5px;
-  backgroundColor: #FFF;
-  alignSelf: stretch;
-  marginBottom: 15px;
-  marginHorizontal: 20px;
-  fontSize: 16px;
-`;
-
-const ErrorMessage = styled.Text`
-  textAlign: center;
-  color: #ce2029;
-  fontSize: 16px;
-  marginBottom: 15px;
-  marginHorizontal: 20px;
-`;
-
-export { Container, Image, Input, ErrorMessage };
+export default StyleSheet.create({
+  loginInput: {
+    paddingHorizontal: "20",
+    paddingVertical: "15",
+    backgroundColor: "#FFFFFF",
+    alignSelf: "stretch",
+    marginBottom: "15",
+    marginHorizontal: "20",
+  },
+});
